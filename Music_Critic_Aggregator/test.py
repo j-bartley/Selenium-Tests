b9c902b3636bbc4086690c6ad04b31a5702b581b
j-bartley <jordan.bartley@hotmail.com>
2023-09-27 20:25:58 -0500

    Added score grab and multi review functionality

--- Music_Critic_Aggregator/test.py
+++ Music_Critic_Aggregator/test.py
@@ -20,29 +20,43 @@ searchBtn.click()
 # Input Search
 searchElem = driver.find_element(By.XPATH, '//*[@id="empty-search-header"]/div/div/div/div/div/form/input')
 driver.implicitly_wait(2)
-searchElem.send_keys("To Pimp A Butterfly")
+albumName = input("Enter an Album Name: ")
+searchElem.send_keys(albumName)
 driver.implicitly_wait(2)
 searchElem.send_keys(Keys.RETURN)
 
 # Find Reviews
 reviewElem = driver.find_element(By.ID, "result-albumreviews")
 reviewCountHolder = reviewElem.find_element(By.CLASS_NAME, "results-count")
-print(reviewCountHolder.text)
+# v This grabs the unedited review count
+# print(reviewCountHolder.text)
 reviewCount = len(reviewCountHolder.text)
 reviewCountString = reviewCountHolder.text
-if reviewCount < 5 and reviewCount > 3:
-    print("More than 10")
-elif reviewCount < 4 and reviewCount > 2:
-    print("Less than 10")
-    reviewCountString = reviewCountString.replace("(", "")
-    reviewCountString = reviewCountString.replace(")", "")
-    print(reviewCountString)
+reviewCountString = reviewCountString.replace("(", "")
+reviewCountString = reviewCountString.replace(")", "")
+print("There are " + reviewCountString + " review(s) available.")
+
+# Open Single Review or Choose From Multiple
+if(reviewCountString == "1"):
+    # Load Review
+    singleReviewElem = driver.find_element(By.XPATH, '//*[@id="result-albumreviews"]/ul/li/div')
+    singleReviewElem.click()
+    # In case of overlay popping up:
+    # overlayCrud = driver.find_element(By.XPATH, '//*[@id="bx-close-inside-1643994"]/svg')
+    # overlayCrud.click()
+    reviewScoreElem = driver.find_element(By.XPATH, '//*[@id="main-content"]/article/div[1]/header/div[1]/div[2]/div/div[2]/div/div[2]/p')
+    reviewScore = reviewScoreElem.text
+    print("The review score for " + albumName + " is " + str(reviewScore))
 else:
-    print("Undefined Reviews")
-#try:
-    
-#except:
-#    print("No Reviews Available")
+    print("Choose Which Review:")
+    multReviewCount = int(reviewCountString)
+    i = 0
+    while i <= multReviewCount:
+        print(i)
+        i = i + 1
 
 # Close Browser
 driver.close()
+
+# Notes:
+# - Album Scores on Pitchfork have different xpath if over 9 score or under 9 score (ex. The Slow Rush by Tame Impala)
